6940050c5344529384c769480fca22983d7c442e
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-10-17 19:52:01 -0500

    Front-end foi atualizado com o jinja

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -1,6 +1,15 @@
+from tkinter import N
 from django.urls import path
-from . import views
+from .views import home, buttons, cards, login, register, forgot_password, error_404, blank, tables
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', home, name='home'),
+    path('buttons', buttons, name='buttons'),
+    path('cards', cards, name='cards'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
+    path('forgot-password', forgot_password, name='forgot-password'),
+    path('error_404', error_404, name='error_404'),
+    path('blank', blank, name='blank'),
+    path('tables', tables, name='tables'),
 ]
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
 
+def login(request):
+    return render(request, 'login.html')
 
-def index(request):
-    return render(request, 'index.html')
+def register(request):
+    return render(request, 'register.html')
+
+def error_404(request):
+    return render(request, '404.html')
+
+def blank(request):
+    return render(request, 'blank.html')
+
+def tables(request):
+    return render(request, 'tables.html')
+
+def forgot_password(request):
+    return render(request, 'forgot-password.html')
+
+def buttons(request):
+    return render(request, 'buttons.html')
+
+def cards(request):
+    return render(request, 'cards.html')
